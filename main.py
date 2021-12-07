c9bcdd1a0fbf0a534912e0125977daac5dd71806
Илья <orskilya@gmail.com>
2021-12-07 23:14:52 +0500

    Тестовые движения и т.д.

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
-print('Hello')
+import pygame
+
+
+class Ship:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_coord(self):
+        return self.x, self.y
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+
+    def draw(self):
+        screen.fill((0, 0, 0))
+        pygame.draw.circle(screen, pygame.Color('yellow'), (500, 500), 100)
+        pygame.draw.circle(screen, pygame.Color('white'), (self.x, self.y), 25)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    fps = 60
+    size = width, height = 1024, 1024
+    screen = pygame.display.set_mode(size)
+    pygame.draw.circle(screen, pygame.Color('yellow'), (500, 500), 100)
+    ship = Ship(100, 100)
+    ship.draw()
+    clock = pygame.time.Clock()
+    move = False
+    running = True
+    while running:
+        if move:
+            ship.move(100 / fps, 100 / fps)
+        ship.draw()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                move = True
+            elif event.type == pygame.KEYUP:
+                move = False
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
