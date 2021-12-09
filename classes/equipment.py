cea4dce4664969dccbb0592cd4dacc9418da34c0
Masaari <maksbelonog78@gmail.com>
2021-12-09 21:54:39 +0500

    Класс оборудования

--- classes/equipment.py
+++ classes/equipment.py
@@ -1,15 +1,48 @@
 class Engine:
-    def __init__(self, speed, jump, price, weight, set=False):
+    def __init__(self, speed, jump, price, mass, set=False):
         self.speed = speed
         self.jump = jump
         self.price = price
-        self.weight = weight
+        self.mass = mass
         self.set = set
 
 
 class FuelTank:
-    def __init__(self, fuel, price, weight, set=False):
+    def __init__(self, fuel, price, mass, set=False):
         self.fuel = fuel
         self.price = price
-        self.weight = weight
+        self.mass = mass
+        self.set = set
+
+
+class Grab:
+    def __init__(self, power, dist, price, mass, set=False):
+        self.power = power
+        self.dist = dist
+        self.price = price
+        self.mass = mass
+        self.set = set
+
+
+class Shield:
+    def __init__(self, defend, price, mass, set=False):
+        self.defend = defend
+        self.price = price
+        self.mass = mass
+        self.set = set
+
+
+class Locator:
+    def __init__(self, dist, price, mass, set=False):
+        self.dist = dist
+        self.price = price
+        self.mass = mass
+        self.set = set
+
+
+class Scaner:
+    def __init__(self, scan, price, mass, set=False):
+        self.scan = scan
+        self.price = price
+        self.mass = mass
         self.set = set
