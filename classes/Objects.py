3f0111d3fcb382d8ff0d7f7b81e6c69ea0157d78
Masaari <maksbelonog78@gmail.com>
2021-12-24 23:23:14 +0500

    Доработка классов объекты и оборудование

--- classes/Objects.py
+++ classes/Objects.py
@@ -1,8 +1,9 @@
+from math import cos, sin
 from random import randrange
 import pygame
 
 products = ('Питание', 'Медикаменты', 'Алкоголь', 'Минералы', 'Роскошь', 'Техника', 'Оружие', 'Наркотики')
-equipments = (1, )  # tuple. Add to main code
+equipments = (1,)  # tuple. Add to main code
 
 
 class Object:
@@ -15,15 +16,20 @@ class Object:
 
 
 class Planet(Object):
-    def __init__(self, coords, image, radius):
+    def __init__(self, coords, image, radius, distance, w, type):
         global products, equipments
         super().__init__(coords, image)
         self.shop = []  # list
-        self.market = {i: [randrange(70, 300), randrange(80, 200)] for i in products}  # name: number, price
+        self.market = {i: [randrange(70, 300), randrange(80, 200), randrange(70, 190)] for i in
+                       products}  # name: number, price
         self.radius = radius
+        self.distance = distance
+        self.w = w  # angular velocity of the planet rotation
 
-    def move(self):
-        pass
+    def move(self, t):
+        x = self.distance * cos(self.w * t)
+        y = self.distance * sin(self.w * t)
+        return x, y
 
     def market_items(self):  # increasing number of the products
         for i in self.market:
