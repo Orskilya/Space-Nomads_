98f8787769e0b33b722873d41d93d92a1e3a95ab
Masaari <maksbelonog78@gmail.com>
2022-01-03 19:31:25 +0500

    Солнечная система. Версия 1.0

--- main.py
+++ main.py
@@ -40,21 +40,38 @@ user32 = ctypes.windll.user32
 size = width, height = user32.GetSystemMetrics(0) - 100, user32.GetSystemMetrics(1) - 100
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+
 # Группы спрайтов
 all_sprites = pygame.sprite.Group()
 planets = pygame.sprite.Group()
 ships = pygame.sprite.Group()
 fps = 60
 camera = Camera()
+
 # Объекты
 sun = Objects.Star(load_image('Sun.png'), 25, 10, [400, 400], [width // 2, height // 2],
                    all_sprites)
-earth = Objects.Planet(load_image('Earth.png'), 50, 5, [200, 200], [width // 2, height // 2], 400,
-                       100, all_sprites)
 mercury = Objects.Planet(load_image('Mercury.png'), 50, 5, [100, 100], [width // 2, height // 2],
-                         200, 100, all_sprites)
-hero_ship = Ships.Ship(load_image('hero_ship.png', (150, 150)), [width // 2, height // 2], 100, 100,
+                         250, 100, all_sprites)
+venus = Objects.Planet(load_image('Venus.png'), 50, 5, [150, 150], [width // 2, height // 2],
+                       400, 100, all_sprites)
+earth = Objects.Planet(load_image('Earth.png'), 50, 5, [200, 200], [width // 2, height // 2], 600,
+                       100, all_sprites)
+mars = Objects.Planet(load_image('Mars.png'), 50, 5, [150, 150], [width // 2, height // 2], 800,
+                      100, all_sprites)
+jupiter = Objects.Planet(load_image('Jupiter.png'), 50, 5, [200, 200], [width // 2, height // 2],
+                         1200, 100, all_sprites)
+saturn = Objects.Planet(load_image('Saturn.png'), 21, 12, [200, 200], [width // 2, height // 2],
+                        1600, 100, all_sprites)
+uranus = Objects.Planet(load_image('Uranus.png'), 50, 5, [200, 200], [width // 2, height // 2], 2000,
+                        100, all_sprites)
+neptune = Objects.Planet(load_image('Neptune.png'), 50, 5, [200, 200], [width // 2, height // 2],
+                         2400, 100, all_sprites)
+
+hero_ship = Ships.Ship(load_image('hero_ship.png', (50, 50)), [width // 2, height // 2], 100, 100,
                        None, all_sprites)
+
+# main cycle
 running = True
 while running:
     for event in pygame.event.get():
@@ -66,9 +83,9 @@ while running:
                 hero_ship.update(event, 'fly')
     screen.fill((0, 0, 0))
     all_sprites.update()
-#     camera.update(hero_ship)
-#     for sprite in all_sprites:
-#         camera.apply(sprite)
+    #     camera.update(hero_ship)
+    #     for sprite in all_sprites:
+    #         camera.apply(sprite)
     all_sprites.draw(screen)
     pygame.display.flip()
     clock.tick(fps)
